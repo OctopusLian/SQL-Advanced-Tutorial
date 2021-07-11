4dd9c991509b5a108c8c3834ee6c681e3c1a9d04
OctopusLian <zoctopus@qq.com>
2021-07-11 17:25:51 +0800

    update ch2 sql

---
+++ ch2/2-5/Teams.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Teams
+(member VARCHAR(32) PRIMARY KEY,
+ team   CHAR(1)     NOT NULL,
+ age    INTEGER     NOT NULL);
+
+INSERT INTO Teams VALUES('��ľ',	'A',	28);
+INSERT INTO Teams VALUES('�ݼ�',	'A',	19);
+INSERT INTO Teams VALUES('����',	'A',	23);
+INSERT INTO Teams VALUES('ɽ��',	'B',	40);
+INSERT INTO Teams VALUES('�ñ�',	'B',	29);
+INSERT INTO Teams VALUES('����',	'C',	30);
+INSERT INTO Teams VALUES('ҰҰ��',      'D',	28);
+INSERT INTO Teams VALUES('��V',	'D',	28);
+INSERT INTO Teams VALUES('����',	'D',	24);
+INSERT INTO Teams VALUES('�³�',	'D',	22);
